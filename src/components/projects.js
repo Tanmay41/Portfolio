49a029acf75c3db9fc3caf509fc893cbf81b021a
Tanmay41 <tanmayror19@gmail.com>
2024-02-26 13:21:29 +0530

    added project 'blogMaster'

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,52 +1,60 @@
 const projects = [
-  {
-    imageUrl:
-      "https://tanmay41.github.io/Portfolio-OLD/assets/images/MoveIt-project.png",
-    link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Movers/",
-    name: "Move-It 📦",
-    description:
-      "Move-It is a website 🌐 that serves as a dynamic online platform, dedicated to facilitating the effortless transfer of individuals' possessions from one location to another. This digital space exemplifies our commitment to providing a user-friendly interface for streamlined and secure moving experiences. 🚚✨",
-  },
-  {
-    imageUrl:
-      "https://tanmay41.github.io/Portfolio-OLD/assets/images/tindog-projct.png",
-    link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Tindog/",
-    name: "Tindog ❤️‍🔥",
-    description:
-      "The TinDog website 🐾, a unique platform akin to Tinder but tailored for our furry friends. This project highlights my creativity in designing a playful yet functional interface for connecting dogs and their owners. 🐶❤️",
-  },
-  {
-    imageUrl:
-      "https://tanmay41.github.io/Portfolio-OLD/assets/images/dice-project.png",
-    link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Dice/",
-    name: "The Dice Game! 🎲",
-    description:
-      "Ever find yourself in a playful disagreement? Let 'The Dice Game' settle the score! 🎲🔥 Roll the virtual dice, and let fate decide who's right. It's not just a game; it's the ultimate arbitrator of friendly disputes! 🤝💬",
-  },
-  {
-    imageUrl:
-      "https://raw.githubusercontent.com/Tanmay41/Portfolio-OLD/main/assets/images/Notie.png",
-    link: "https://wwspt8.csb.app/",
-    name: "Notie App 📝",
-    description:
-      "Notie App is a simple note-taking application 📝. Perfect for jotting down quick thoughts and ideas.",
-  },
-  {
-    imageUrl:
-      "https://raw.githubusercontent.com/Tanmay41/Portfolio-OLD/main/assets/images/TODO.png",
-    link: "https://tj9c3l.csb.app/",
-    name: "Todo List ✅",
-    description:
-      "A Todo List application to help you stay organized and manage your tasks efficiently. ✅",
-  },
-  {
-    imageUrl:
-      "https://tanmay41.github.io/Portfolio-OLD/assets/images/Calculator.png",
-    link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Calculator",
-    name: "Calculator 🧮",
-    description:
-      "A simple calculator application for basic arithmetic operations.",
-  },
+	{
+		imageUrl:
+			"https://tanmay41.github.io/Portfolio-OLD/assets/images/MoveIt-project.png",
+		link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Movers/",
+		name: "Move-It 📦",
+		description:
+			"Move-It is a website 🌐 that serves as a dynamic online platform, dedicated to facilitating the effortless transfer of individuals' possessions from one location to another. This digital space exemplifies our commitment to providing a user-friendly interface for streamlined and secure moving experiences. 🚚✨",
+	},
+	{
+		imageUrl:
+			"https://tanmay41.github.io/Portfolio-OLD/assets/images/tindog-projct.png",
+		link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Tindog/",
+		name: "Tindog ❤️‍🔥",
+		description:
+			"The TinDog website 🐾, a unique platform akin to Tinder but tailored for our furry friends. This project highlights my creativity in designing a playful yet functional interface for connecting dogs and their owners. 🐶❤️",
+	},
+	{
+		imageUrl:
+			"https://tanmay41.github.io/Portfolio-OLD/assets/images/dice-project.png",
+		link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Dice/",
+		name: "The Dice Game! 🎲",
+		description:
+			"Ever find yourself in a playful disagreement? Let 'The Dice Game' settle the score! 🎲🔥 Roll the virtual dice, and let fate decide who's right. It's not just a game; it's the ultimate arbitrator of friendly disputes! 🤝💬",
+	},
+	{
+		imageUrl:
+			"https://raw.githubusercontent.com/Tanmay41/Portfolio-OLD/main/assets/images/Notie.png",
+		link: "https://wwspt8.csb.app/",
+		name: "Notie App 📝",
+		description:
+			"Notie App is a simple note-taking application 📝. Perfect for jotting down quick thoughts and ideas.",
+	},
+	{
+		imageUrl:
+			"https://raw.githubusercontent.com/Tanmay41/Portfolio-OLD/main/assets/images/TODO.png",
+		link: "https://tj9c3l.csb.app/",
+		name: "Todo List ✅",
+		description:
+			"A Todo List application to help you stay organized and manage your tasks efficiently. ✅",
+	},
+	{
+		imageUrl:
+			"https://tanmay41.github.io/Portfolio-OLD/assets/images/Calculator.png",
+		link: "https://tanmay41.github.io/Portfolio-OLD/assets/Projects/Calculator",
+		name: "Calculator 🧮",
+		description:
+			"A simple calculator application for basic arithmetic operations.",
+	},
+	{
+		imageUrl:
+			"https://tanmay41.github.io/Portfolio-OLD/assets/images/blogMaster.png",
+		link: "https://blogmaster-m0a5.onrender.com/",
+		name: "Blog Master📝",
+		description:
+			"A sophisticated blog website created using React, backed by an API implemented with Express for fetching and sending data to the client.",
+	},
 ];
 
 export default projects;
