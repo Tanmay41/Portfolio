e8f38cfd34325cbaf74f8e05818e5f1ab78bece6
Tanmay <131357384+Tanmay41@users.noreply.github.com>
2024-10-04 22:05:47 +0530

    new nextjs portfolio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,10 @@
 import type { Config } from "tailwindcss";
-const colors = require("tailwindcss/colors");
 const {
 	default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
+import svgToDataUri from "mini-svg-data-uri";
 
-const svgToDataUri = require("mini-svg-data-uri");
-
-// @ts-ignore
-// @ts-ignore
-const config = {
+const config: Config = {
 	darkMode: ["class"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
@@ -43,100 +39,82 @@ const config = {
 					},
 				},
 				moveInCircle: {
-					"0%": {
-						transform: "rotate(0deg)",
-					},
-					"50%": {
-						transform: "rotate(180deg)",
-					},
-					"100%": {
-						transform: "rotate(360deg)",
-					},
+					"0%": { transform: "rotate(0deg)" },
+					"50%": { transform: "rotate(180deg)" },
+					"100%": { transform: "rotate(360deg)" },
 				},
 				moveVertical: {
+					"0%": { transform: "translateY(-50%)" },
+					"50%": { transform: "translateY(50%)" },
+					"100%": { transform: "translateY(-50%)" },
+				},
+				meteor: {
 					"0%": {
-						transform: "translateY(-50%)",
+						transform: "rotate(215deg) translateX(0)",
+						opacity: "1",
 					},
-					"50%": {
-						transform: "translateY(50%)",
-					},
+					"70%": { opacity: "1" },
 					"100%": {
-						transform: "translateY(-50%)",
-					},
-					aspectRatio: {
-						// @ts-ignore
-						project: "667 / 375",
-					},
-					"accordion-down": {
-						from: { height: 0 },
-						to: { height: "var(--radix-accordion-content-height)" },
-					},
-					"accordion-up": {
-						from: {
-							height: "var(--radix-accordion-content-height)",
-						},
-						to: { height: 0 },
-					},
-					meteor: {
-						"0%": {
-							transform: "rotate(215deg) translateX(0)",
-							opacity: "1",
-						},
-						"70%": { opacity: "1" },
-						"100%": {
-							transform: "rotate(215deg) translateX(-900px)",
-							opacity: "0",
-						},
+						transform: "rotate(215deg) translateX(-900px)",
+						opacity: "0",
 					},
 				},
-				animation: {
-					"accordion-down": "accordion-down 0.2s ease-out",
-					"accordion-up": "accordion-up 0.2s ease-out",
-					"meteor-effect": "meteor 5s linear infinite",
-					first: "moveVertical 30s ease infinite",
-					second: "moveInCircle 20s reverse infinite",
-					third: "moveInCircle 40s linear infinite",
-					fourth: "moveHorizontal 40s ease infinite",
-					fifth: "moveInCircle 20s ease infinite",
+				accordionDown: {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
 				},
+				accordionUp: {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				accordionDown: "accordionDown 0.2s ease-out",
+				accordionUp: "accordionUp 0.2s ease-out",
+				meteorEffect: "meteor 5s linear infinite",
+				first: "moveVertical 30s ease infinite",
+				second: "moveInCircle 20s reverse infinite",
+				third: "moveInCircle 40s linear infinite",
+				fourth: "moveHorizontal 40s ease infinite",
+				fifth: "moveInCircle 20s ease infinite",
 			},
 		},
-		plugins: [
-			require("tailwindcss-animate"),
-			require("@designbycode/tailwindcss-text-stroke"),
-			addVariablesForColors,
-			function ({ matchUtilities, theme }: any) {
-				matchUtilities(
-					{
-						"bg-grid": (value: any) => ({
-							backgroundImage: `url("${svgToDataUri(
-								`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-							)}")`,
-						}),
-						"bg-grid-small": (value: any) => ({
-							backgroundImage: `url("${svgToDataUri(
-								`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-							)}")`,
-						}),
-						"bg-dot": (value: any) => ({
-							backgroundImage: `url("${svgToDataUri(
-								`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-							)}")`,
-						}),
-					},
-					{
-						values: flattenColorPalette(theme("backgroundColor")),
-						type: "color",
-					}
-				);
-			},
-		],
 	},
-} satisfies Config;
+	plugins: [
+		require("tailwindcss-animate"),
+		require("@designbycode/tailwindcss-text-stroke"),
+		addVariablesForColors,
+		function ({ matchUtilities, theme }: any) {
+			matchUtilities(
+				{
+					"bg-grid": (value: string) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+						)}")`,
+					}),
+					"bg-grid-small": (value: string) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+						)}")`,
+					}),
+					"bg-dot": (value: string) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+						)}")`,
+					}),
+				},
+				{
+					values: flattenColorPalette(theme("backgroundColor")),
+					type: "color",
+				}
+			);
+		},
+	],
+};
 
 function addVariablesForColors({ addBase, theme }: any) {
-	let allColors = flattenColorPalette(theme("colors"));
-	let newVars = Object.fromEntries(
+	const allColors = flattenColorPalette(theme("colors"));
+	const newVars = Object.fromEntries(
 		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
 	);
 
