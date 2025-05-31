d5a67f26d1f971fce3a80a96701ecb0013378aa5
Tanmay <131357384+Tanmay41@users.noreply.github.com>
2025-05-31 19:13:05 +0530

    NAS - fixed ' error with &apos;

--- app/NAS/page.tsx
+++ app/NAS/page.tsx
@@ -4,11 +4,11 @@ export default function NAS() {
       <h1 className="text-2xl font-bold text-blue-700 mb-4">👓 AI Glasses for the Blind</h1>
       
       <p className="text-gray-800 text-base">
-        AI glasses for the blind that acs as an assistance to the user(blind), on press of a button it snaps a picture from the camera(esp32-cam) on the glasses and uses AI to detect and describe the scene in front of the user, and the best part is that it just costs me like $10 USD! I want to make it work in realtime with a mic and be like a helping person to the blind user but couldn't do it because of my low buget
+        AI glasses for the blind that acs as an assistance to the user(blind), on press of a button it snaps a picture from the camera(esp32-cam) on the glasses and uses AI to detect and describe the scene in front of the user, and the best part is that it just costs me like $10 USD! I want to make it work in realtime with a mic and be like a helping person to the blind user but couldn&apos;t do it because of my low buget
       </p>
 
       <p className="text-sm italic text-gray-600 mt-4">
-        ⚠️ As it’s an IoT hardware project, I can't host it online. But I’ve added it to my website so I can tell you about it.
+        ⚠️ As it&apos;s an IoT hardware project, I can&apos;t host it online. But I&apos;ve added it to my website so I can tell you about it.
       </p>
     </div>
   );
